3588fefa1daaaa888bc3022af9d8eae60f1ec7ae
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-01-08 11:04:23 +0100

    fetch

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,7 +6,8 @@ setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   git init
-  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/Skarlso/skarlso.github.io.git > /dev/null 2>&1
+  git remote add origin https://${GITHUB_TOKEN}@github.com/Skarlso/skarlso.github.io.git > /dev/null 2>&1
+  git fetch origin
 }
 
 commit_website_files() {
@@ -15,7 +16,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git push --quiet --set-upstream origin-pages master
+  git push --quiet --set-upstream origin master
 }
 
 cd /opt/blog
