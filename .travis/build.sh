2ceb03c06e530ca838fd47cbc0c60536fcd16980
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-01-08 10:40:19 +0100

    Changing to travis.

---
+++ .travis/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+mkdir /opt/blog
+git clone https://github.com/Skarlso/blogsource.git /opt/app
+echo Build started on `date`
+cd /opt/app
+hugo --theme purehugo
+cp -R public/* /opt/blog
