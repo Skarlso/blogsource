9dfeee90bceecc2841b1b392b717cadcc92cbea5
Gergely Brautigam <skarlso777@gmail.com>
2019-09-24 23:21:35 +0200

    Removed autohiding and scrolljacking.

--- themes/hermit/assets/js/main.js
+++ themes/hermit/assets/js/main.js
@@ -2,20 +2,6 @@
  * Utils
  */
 
-// Throttle
-//
-const throttle = (callback, limit) => {
-  let timeoutHandler = null;
-  return () => {
-    if (timeoutHandler == null) {
-      timeoutHandler = setTimeout(() => {
-        callback();
-        timeoutHandler = null;
-      }, limit);
-    }
-  };
-};
-
 // addEventListener Helper
 //
 const listen = (ele, e, callback) => {
@@ -32,18 +18,6 @@ const listen = (ele, e, callback) => {
 //
 let header = document.getElementById('site-header');
 let lastScrollPosition = window.pageYOffset;
-
-const autoHideHeader = () => {
-  let currentScrollPosition = window.pageYOffset;
-  if (currentScrollPosition > lastScrollPosition) {
-    header.classList.remove('slideInUp');
-    header.classList.add('slideOutDown');
-  } else {
-    header.classList.remove('slideOutDown');
-    header.classList.add('slideInUp');
-  }
-  lastScrollPosition = currentScrollPosition;
-}
 
 // Mobile Menu Toggle
 //
@@ -85,12 +59,4 @@ if (header !== null) {
   listen('#toc-btn', "click", toggleToc);
   listen('#img-btn', "click", showImg);
   listen('.bg-img', "click", hideImg);
-
-  window.addEventListener('scroll', throttle(() => {
-    autoHideHeader();
-
-    if (mobileMenuVisible == true) {
-      toggleMobileMenu();
-    }
-  }, 250));
 }
